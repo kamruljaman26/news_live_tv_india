f8bd4a4c9ff20754753b618c4cdc6c7dde7c1f48
Kamrul Jaman <kamruljaman@Kamruls-MacBook-Pro.local>
2020-10-05 22:36:02 +0600

    Added Empty Home,TV Activity & create structure

--- app/src/main/java/com/example/livenewstvindia/MainActivity.java
+++ app/src/main/java/com/example/livenewstvindia/MainActivity.java
@@ -2,13 +2,40 @@ package com.example.livenewstvindia;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 
+import com.example.livenewstvindia.view.home.HomeActivity;
+
+/**
+ * Splash Screen Activity
+ */
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Hold the activity for 2-3 second
+        Thread thread = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    Thread.sleep(2000);
+                    gotoHomePage();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+        thread.start();
+
+    }
+
+    //Goto another activity
+    private void gotoHomePage() {
+        Intent intent = new Intent(MainActivity.this, HomeActivity.class);
+        startActivity(intent);
     }
 }
---
+++ app/src/main/java/com/example/livenewstvindia/view/tv/TvActivity.java
@@ -0,0 +1,16 @@
+package com.example.livenewstvindia.view.tv;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.livenewstvindia.R;
+
+public class TvActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tv);
+    }
+}
